999f5670fe49cf0b67288bde18080d573caf1eaa
ladynatalybr <150546378+ladynatalybr@users.noreply.github.com>
2023-12-06 19:55:43 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    include('config/config.php');
+?>
+
+<DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF=8"/>
+<title> Administrador de pacientes</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include('menu.php')?>
+    <div class="container">
+        <h1 class="text-center mt-5">Registro pacientes.</h1>
+    </div>
+</body>
+
+</html>
